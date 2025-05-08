acc773e5b0cae4e75041c39fc960f859e3603fd6
yolonime <ti.boug.95100@gmail.com>
2025-05-08 19:11:43 +0200

    search bar

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View, FlatList, TouchableOpacity } from "react-native";
+import { Text, View, FlatList, TouchableOpacity, TextInput } from "react-native";
 import { useState, useEffect } from "react";
 import { supabase } from '@/utils/supabase';
 import { getProductImageUrl } from '@/services/productService';
@@ -13,6 +13,8 @@ export interface Produit {
 
 export default function Index() {
   const [produits, setProduits] = useState<Produit[]>([]);
+  const [searchQuery, setSearchQuery] = useState(''); // État pour la barre de recherche
+  const [filteredProduits, setFilteredProduits] = useState<Produit[]>([]); // Produits filtrés
 
   useEffect(() => {
     const fetchProduits = async () => {
@@ -26,16 +28,34 @@ export default function Index() {
       }
 
       setProduits(data);
+      setFilteredProduits(data); // Initialiser les produits filtrés
     };
 
     fetchProduits();
   }, []);
 
+  // Mettre à jour les produits filtrés en fonction de la recherche
+  useEffect(() => {
+    const filtered = produits.filter((produit) =>
+      produit.nom.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    setFilteredProduits(filtered);
+  }, [searchQuery, produits]);
+
   return (
     <View className="flex-1 bg-white px-4 py-6">
       <Text className="text-center text-2xl font-bold mb-6">OUR SOLUTIONS</Text>
+
+      {/* Barre de recherche */}
+      <TextInput
+        className="bg-gray-200 rounded-lg px-4 py-2 mb-4"
+        placeholder="Rechercher un produit..."
+        value={searchQuery}
+        onChangeText={setSearchQuery}
+      />
+
       <FlatList
-        data={produits}
+        data={filteredProduits} // Utiliser les produits filtrés
         numColumns={2}
         keyExtractor={(item) => item.id_produit.toString()}
         columnWrapperStyle={{ justifyContent: 'space-between' }}
@@ -44,7 +64,7 @@ export default function Index() {
 
           return (
             <TouchableOpacity className="w-[48%] bg-gray-100 rounded-lg items-center justify-center p-4 mb-4 shadow-md">
-              <ProductImage uri={imageUrl}  />
+              <ProductImage uri={imageUrl} />
               <Text className="text-center text-lg font-semibold">{item.nom}</Text>
             </TouchableOpacity>
           );
